7fc5c90dd5ad61a75a9ca4e64e954a2182aaa61b
Guido Schmutz <guido.schmutz@trivadis.com>
2019-04-23 19:13:54 +0200

    update properties

--- kafka/kafka-backup/impl/scripts/start-spredfast-s3-source.sh
+++ kafka/kafka-backup/impl/scripts/start-spredfast-s3-source.sh
@@ -9,13 +9,17 @@ curl -X "POST" "$DOCKER_HOST_IP:8083/connectors" \
   "config": {
       "connector.class": "com.spredfast.kafka.connect.s3.source.S3SourceConnector",
       "tasks.max": "1",
-      "s3.region": "eu-central-1",
       "s3.bucket": "gschmutz-kafka-spredfast-1",
+      "s3.prefix": "test",
+      "s3.start.marker": "2019-04-23",
+      "s3.page.size": "100"
       "topics": "truck_position",
+      "targetTopic.truck_position": "truck_position"
+      "max.partition.count": "200",
 	    "key.converter": "com.spredfast.kafka.connect.s3.AlreadyBytesConverter",
       "value.converter": "com.spredfast.kafka.connect.s3.AlreadyBytesConverter",
       "format": "binary",
       "format.include.keys": "true",
-      "s3.start.marker": "2019-04-23"
+      "max.poll.records": "1000",
   }
 }'
